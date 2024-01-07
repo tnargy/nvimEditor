74998cec59cd4f52f8e2f1178c0597f37d1743c1
onyx <clsscmstng@gmail.com>
2024-01-07 15:07:23 -0600

    Fixed lsp and added tab remaps

--- lua/ggandy/remap.lua
+++ lua/ggandy/remap.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete)
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
