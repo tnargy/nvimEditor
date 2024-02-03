b256929d3d46ec42c657cf22fbfb51d92a8e58a3
Grant <clsscmstng@gmail.com>
2024-02-03 14:22:11 +0000

    color, snippets, completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,17 +9,29 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
+                auto_install = true,
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+            local lspconfig = require("lspconfig")
+    -- ◍ angular-language-server angularls (keywords: angular)
+    -- ◍ clangd (keywords: c, c++)
+    -- ◍ lua-language-server lua_ls (keywords: lua)
+    -- ◍ typescript-language-server tsserver (keywords: typescript, javascript)
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities
+            })
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
